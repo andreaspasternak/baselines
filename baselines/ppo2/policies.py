a8505360d0a2f1858c1aaf9d73adfcb5ea858ab0
Andreas Pasternak <andreas.pasternak@gmx.ch>
2018-09-19 02:01:20 +0200

    ------------------------------------- | approxkl           | 0.029566338  | | clipfrac           | 0.08479004   | | eplenmean          | 0            | | eprewmean          | 0.915        | | explained_variance | 0.843        | | fps                | 403          | | learning_rate      | 9.57e-06     | | nupdates           | 30           | | policy_entropy     | 0.17407344   | | policy_loss        | 0.0008952819 | | serial_timesteps   | 491520       | | time_elapsed       | 6.29e+03     | | total_rew          | 1.5e+04      | | total_timesteps    | 2457600      | | value_loss         | 0.057825506  | ------------------------------------- Saving to /home/pasa/deeplearning/tf_models/snake/2018-09-18_23-45-46/checkpoints/00030 (16384, 5) 15102.0

--- baselines/ppo2/policies.py
+++ baselines/ppo2/policies.py
@@ -16,8 +16,10 @@ def nature_cnn(unscaled_images, **conv_kwargs):
                    **conv_kwargs))
     h2 = activ(conv(h, 'c2', nf=64, rf=3, stride=1, init_scale=np.sqrt(2), **conv_kwargs))
     h3 = activ(conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2), **conv_kwargs))
+    h3 = activ(conv(h2, 'c4', nf=64, rf=3, stride=1, init_scale=np.sqrt(2), **conv_kwargs))
     h3 = conv_to_fc(h3)
-    h3 = activ(fc(h3, 'fc1', nh=2048, init_scale=np.sqrt(2)))
+    h3 = activ(fc(h3, 'fc1', nh=2048*2, init_scale=np.sqrt(2)))
+    h3 = activ(fc(h3, 'fc11', nh=2048, init_scale=np.sqrt(2)))
 
     raw = tf.layers.flatten(scaled_images)
     raw = activ(fc(raw, 'fcr1', nh=2048, init_scale=np.sqrt(2)))
